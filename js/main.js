0536e9e48a1401c4c460cff54e579d59ce99122c
Kichkina dev <95954469+Kichkina-dev@users.noreply.github.com>
2024-10-13 16:04:19 +0500

    32

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // API URLlari
-const searchApiUrl = "https://147.45.106.35:8000/api/types/search/";
-const analyseApiUrl = "https://147.45.106.35:8000/api/analyses/";
+const searchApiUrl = "http://147.45.106.35:8000/api/types/search/";
+const analyseApiUrl = "http://147.45.106.35:8000/api/analyses/";
 
-if (window.location.protocol === 'http:') {
+if (window.location.protocol === 'https:') {
     window.location.href = window.location.href.replace('http:', 'https:');
 }
 
